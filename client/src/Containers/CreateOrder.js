928b22bd7ddc1e0d6f458e055b52ceaccbb590a8
Muhammad Salman Sarwar <muhammadsalmansarwar32@gmail.com>
2023-07-15 22:23:55 +0500

    fix

--- client/src/Containers/CreateOrder.js
+++ client/src/Containers/CreateOrder.js
@@ -102,7 +102,7 @@ function CreateOrder() {
 
     async function fetchProductDetail() {
         const response = await axios
-            .get(`https://eprime-store.vercel.app/products/${productId}`)
+            .get(`https://eprime-store-api.vercel.app/products/${productId}`)
             .catch((err) => {
                 console.log(err);
             });
@@ -123,7 +123,7 @@ function CreateOrder() {
     const handleSubmit = (e) => {
         e.preventDefault();
         const order = {
-            productLink: `https://eprime-store-api.vercel.app/products/${productId}`,
+            productLink: `https://eprime-store.vercel.app/products/${productId}`,
             customerName,
             customerNumber: phoneNumber,
             address,
